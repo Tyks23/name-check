276633116f6ae879f3d610da99beaf43c9b76d96
Tyks23 <epic.raider99@gmail.com>
2022-01-11 16:42:16 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+	let name = "";
+</script>
+<form>
+	<input type="text" bind:value="{name}" placeholder="enter name here">
+</form>
+<h1>Your name: {name}</h1>
+<p>The maximum allowed length of a name is 5 characters, your name is {name.length} characters long</p>
+<p>Which is <b>{name.length <= 5 ? 'ACCEPTABLE' : 'UNACCEPTABLE'}</b></p>
